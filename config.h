a820e8c15699793ebe3640b1921a52fc59fa72cc
Edvin Källström <edvin.kallstrom@protonmail.com>
2025-06-01 19:19:01 +0200

    Update layout

--- config.h
+++ config.h
@@ -14,7 +14,7 @@ static const char unknown_str[] = "n/a";
 static const struct arg args[] = {
 	/* function, format, argument */
     { run_command, "%s ", "mpc --format '%artist% - %title%' | head -1"},
-    { run_command, "%s | ", "mpc status 'nr: %songpos% \\[%currenttime%/%totaltime%\\] %volume% Shuffle: %random%'"},
+    { run_command, "%s | ", "mpc status '(%currenttime%/%totaltime%) Vol: %volume% Shuffle: %random%'"},
     { cpu_perc,     "CPU: %s%% | ", "NULL" },
     { ram_used,      "RAM: %sB | ", "NULL" },
     { run_command,         "%s | ", "cat ~/keyboardlayout" },
